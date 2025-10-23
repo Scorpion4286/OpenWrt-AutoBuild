0248ea665f1fdfced614991de125825cd0d68d1a
Scorpion4286 <Scorpion4286@outlook.com>
2025-10-23 21:10:07 +0800

    update

--- diy.sh
+++ diy.sh
@@ -2,30 +2,12 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX https://p3terx.com
+# Author: eSirPlayground
 # Modify: Scorpion4286
 #=================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/172.16.100.15/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/172.16.10.251/g' openwrt/package/base-files/files/bin/config_generate
 # Change NTP Server
-sed -i 's/0.openwrt.pool.ntp.org/cn.ntp.org.cn/g' package/base-files/files/bin/config_generate
-sed -i 's/1.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
-
-# Copy config files
-cp -f -r ./files ./package/base-files
-
-# Install AdGuard Home
-cd ./package
-git clone https://github.com/kongfl888/luci-app-adguardhome.git
-cd ./luci-app-adguardhome
-git checkout v1.8-20200824
-cd ..
-
-# Install Clash
-git clone https://github.com/vernesong/OpenClash.git
-cd ./OpenClash
-git checkout v0.41.14-beta
-cd ..
-
-cd .. && ./scripts/feeds install -a
+sed -i 's/0.openwrt.pool.ntp.org/cn.ntp.org.cn/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp.aliyun.com/g' openwrt/package/base-files/files/bin/config_generate
